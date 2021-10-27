59c820e08a6c36243c9095328e33cfcda68ddd3f
ekopania <ekopania4@gmail.com>
2021-10-26 22:46:51 -0600

    update hist

--- SALMON/22_exp_diff_hist.r
+++ SALMON/22_exp_diff_hist.r
@@ -4,7 +4,7 @@ library(biomaRt)
 library(vegan)
 library(ggplot2)
 
-dataset<-"Yintro_exp1"
+dataset<-"Yintro_exp2"
 cell_type<-"RS"
 procoOnly<-FALSE
 
@@ -81,11 +81,11 @@ for(c in names(cross_types)){
 	df_y<-as.data.frame(cbind(Y=y_diff))
 	#print(head(df_auto))
 	#Setting y=..count../sum(..count..)) should make the y=axis of histogram proportion of total
-	p<-ggplot(df_auto, aes(x)) + geom_histogram(aes(x=auto, y=..count../sum(..count..)), fill="black")
-	p<-p + geom_histogram(data=df_x, aes(x=X, y=-..count../sum(..count..)), fill="lightgrey", alpha=0.5)
-	p<-p + geom_histogram(data=df_y, aes(x=Y, y=-..count../sum(..count..)), fill="darkgrey", alpha=0.5)
+	p<-ggplot(df_auto, aes(x)) + geom_histogram(aes(x=auto, y=..count../sum(..count..)), fill="black", binwidth=diff(range(df_auto$auto))/30)
+	p<-p + geom_histogram(data=df_x, aes(x=X, y=-..count../sum(..count..)), fill="lightgrey", alpha=0.5, binwidth=diff(range(df_auto$auto))/30)
+	p<-p + geom_histogram(data=df_y, aes(x=Y, y=-..count../sum(..count..)), fill="darkgrey", alpha=0.5, binwidth=diff(range(df_auto$auto))/30)
 	p<-p + labs(title=paste("Histogram of normalized gene expression:\n",cross_types[[c]][1],"vs",cross_types[[c]][2]), x="Normalized FPKM Difference", y="Proportion of Genes")
-	p<-p + theme_minimal()
+	p<-p + theme_minimal() + xlim(-0.6, 0.6) + ylim(-0.3, 0.3)
 	p<-p + theme(axis.text=element_text(size=18), axis.title=element_text(size=21), plot.title=element_text(size=22))
 	print(p + coord_flip())
 	#Kolmogorov-Smirnov tests
@@ -97,5 +97,7 @@ for(c in names(cross_types)){
 	print(ks.test(df_x$X, df_y$Y))
 }
 dev.off()
+
+warnings()
 
 print("Done with 22_exp_diff_hist.r")

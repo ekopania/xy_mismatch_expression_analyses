82ee8142c8edcd93a6f76bc395a8e4fc8f9300fa
ekopania <ekopania4@gmail.com>
2021-10-27 23:51:43 -0600

    add plot exp by copy number and upset plot

--- SALMON/09_plot_exp_by_copyNum.r
+++ SALMON/09_plot_exp_by_copyNum.r
@@ -4,6 +4,72 @@
 #3) Plot TPM by relative copy number ratios (ex: Does Slx:Sly copy number ratio correlated w/ expression levels of each gene family?)
 	#NOTE: Doing correlation on a ratio might be problematic - maybe bin these and do a Wilcox test for Fisher's exact test? Should be easy to bin b/c Y-intro lines should be way off but pure strains should be the same; I guess this is kinda the same as doing a Wilcox test between the various cross types so maybe unnecessary
 
+library(biomaRt)
+library(ggplot2)
 
+#Read in tpm data
+exp1_tpms<-read.table("gene_family_tpms.Yintro_exp1.RS.97.txt", header=TRUE)
+exp2_tpms<-read.table("gene_family_tpms.Yintro_exp2.RS.97.txt", header=TRUE)
+larson_tpms<-read.table("../SALMON_LarsonEtAl2017/gene_family_tpms.LarsonEtal.RS.97.txt", header=TRUE)
 
+#Read in copy number data
+cn<-read.table("/mnt/beegfs/ek112884/cnvs/AMPLICONE_ANALYSES/OWN_INFOSITE_METHOD/cnv_estimates.amplicone.combined.txt", header=FALSE)
 
+#Get geneIDs
+ens_mus<-useMart(biomart="ENSEMBL_MART_ENSEMBL", dataset="mmusculus_gene_ensembl")
+all_genes<-getBM(attributes=c('ensembl_gene_id','external_gene_name','chromosome_name','start_position','end_position'), mart=ens_mus)
+colnames(all_genes)<-c("gene_id", "gene_name", "chr", "start", "end")
+print(head(all_genes))
+
+#Vector of gene families to include in analysis
+#gene_fams<-c("atakusan","speer","astx","slx","slxl1","asty","sly","ssty1","ssty2","eif2s3x","eif2s3y")
+gene_fams<-c("slx","sly")
+
+#Estimate gene family tpms
+exp1_gene_fam_tpms<-c()
+exp2_gene_fam_tpms<-c()
+larson_gene_fam_tpms<-c()
+for(g in gene_fams){
+        print(paste("Working on gene family",g))
+        #Get gene IDs for all paralogs in gene family
+        my_bed<-read.table(paste0("../GENE_FAMILY_FILES/",g,".pID",paralog_thresh,".gene_family_paralogs.bed"), header=FALSE, sep="\t")
+        gene_names<-as.character(my_bed[,6])
+        print(head(gene_names))
+        gene_ids<-all_genes$gene_id[which(all_genes$gene_name %in% gene_names)]
+        print(head(gene_ids))
+        #Sum tpms - exp1
+        my_tpms<-exp1_tpms[which(rownames(exp1_tpms) %in% gene_ids),]
+        if(is.vector(my_tpms)){ #Dealing with weird quirk if exactly one gene matches myDGE and gene_ids
+                my_tpms<-as.data.frame(rbind(my_tpms, rep(0,length(my_tpms))))
+        }
+        print(dim(my_tpms))
+        my_sums<-colSums(my_tpms)
+        exp1_gene_fam_tpms<-rbind(exp1_gene_fam_tpms,my_sums)
+	#Sum tpms - exp2
+        my_tpms<-exp2_tpms[which(rownames(exp2_tpms) %in% gene_ids),]
+        if(is.vector(my_tpms)){ #Dealing with weird quirk if exactly one gene matches myDGE and gene_ids
+                my_tpms<-as.data.frame(rbind(my_tpms, rep(0,length(my_tpms))))
+        }
+        print(dim(my_tpms))
+        my_sums<-colSums(my_tpms)
+	exp2_gene_fam_tpms<-rbind(exp2_gene_fam_tpms,my_sums)
+	#Sum tpms - larson
+        my_tpms<-larson_tpms[which(rownames(larson_tpms) %in% gene_ids),]
+        if(is.vector(my_tpms)){ #Dealing with weird quirk if exactly one gene matches myDGE and gene_ids
+                my_tpms<-as.data.frame(rbind(my_tpms, rep(0,length(my_tpms))))
+        }
+        print(dim(my_tpms))
+        my_sums<-colSums(my_tpms)
+	larson_gene_fam_tpms<-rbind(larson_gene_fam_tpms,my_sums)
+}
+rownames(exp1_gene_fam_tpms)<-gene_fams
+print(dim(exp1_gene_fam_tpms))
+print(exp1_gene_fam_tpms)
+rownames(exp2_gene_fam_tpms)<-gene_fams
+print(dim(exp2_gene_fam_tpms))
+print(exp2_gene_fam_tpms)
+rownames(larson_gene_fam_tpms)<-gene_fams
+print(dim(larson_gene_fam_tpms))
+print(larson_gene_fam_tpms)
+
+print("Done with 09_plot_exp_by_copyNum.r")
---
+++ SALMON/23_upset_plot.r
@@ -0,0 +1,116 @@
+#PURPOSE: Generate upset plots for DE genes shared among cross types
+
+library(UpSetR)
+
+#Read in data
+cell_type<-"RS"
+
+print(paste("Reading in DE genes for cell type:", cell_type))
+
+exp1_musX<-read.table(paste0("topDEgenes.lrt.DE.CCPP_",cell_type,"vsCCPPLY_",cell_type,".txt"), header=TRUE)
+exp1_musY<-read.table(paste0("topDEgenes.lrt.DE.CCPP_",cell_type,"vsWWLLPY_",cell_type,".txt"), header=TRUE)
+exp1_domX<-read.table(paste0("topDEgenes.lrt.DE.WWLL_",cell_type,"vsWWLLPY_",cell_type,".txt"), header=TRUE)
+exp1_domY<-read.table(paste0("topDEgenes.lrt.DE.CCPPLY_",cell_type,"vsWWLL_",cell_type,".txt"), header=TRUE)
+
+exp2_musX<-read.table(paste0("topDEgenes.lrt.DE.PPLL_",cell_type,"vsPPLLPY_",cell_type,".txt"), header=TRUE)
+exp2_musY<-read.table(paste0("topDEgenes.lrt.DE.LLPP_",cell_type,"vsPPLLPY_",cell_type,".txt"), header=TRUE)
+exp2_domX<-read.table(paste0("topDEgenes.lrt.DE.LLPP_",cell_type,"vsLLPPLY_",cell_type,".txt"), header=TRUE)
+exp2_domY<-read.table(paste0("topDEgenes.lrt.DE.LLPPLY_",cell_type,"vsPPLL_",cell_type,".txt"), header=TRUE)
+
+larson_musX<-read.table(paste0("../SALMON_LarsonEtAl2017/topDEgenes.lrt.DE.CCPP_",cell_type,"vsPPLL_",cell_type,".txt"), header=TRUE)
+larson_musY<-read.table(paste0("../SALMON_LarsonEtAl2017/topDEgenes.lrt.DE.CCPP_",cell_type,"vsLLPP_",cell_type,".txt"), header=TRUE)
+larson_domX<-read.table(paste0("../SALMON_LarsonEtAl2017/topDEgenes.lrt.DE.LLPP_",cell_type,"vsWWLL_",cell_type,".txt"), header=TRUE)
+larson_domY<-read.table(paste0("../SALMON_LarsonEtAl2017/topDEgenes.lrt.DE.PPLL_",cell_type,"vsWWLL_",cell_type,".txt"), header=TRUE)
+
+print("Separating by direction and chromosome type")
+#Get that are consistently DE in the same direction; higher means higher exp in hybrid or XY mismatch
+exp1_musX_higher<-rownames(exp1_musX)[which(exp1_musX$direc=="-")]
+exp2_musX_higher<-rownames(exp2_musX)[which(exp2_musX$direc=="+")]
+larson_musX_higher<-rownames(larson_musX)[which(larson_musX$direc=="-")]
+exp1_musX_lower<-rownames(exp1_musX)[which(exp1_musX$direc=="+")]
+exp2_musX_lower<-rownames(exp2_musX)[which(exp2_musX$direc=="-")]
+larson_musX_lower<-rownames(larson_musX)[which(larson_musX$direc=="+")]
+exp1_domX_higher<-rownames(exp1_domX)[which(exp1_domX$direc=="-")]
+exp2_domX_higher<-rownames(exp2_domX)[which(exp2_domX$direc=="+")]
+larson_domX_higher<-rownames(larson_domX)[which(larson_domX$direc=="+")]
+exp1_domX_lower<-rownames(exp1_domX)[which(exp1_domX$direc=="+")]
+exp2_domX_lower<-rownames(exp2_domX)[which(exp2_domX$direc=="-")]
+larson_domX_lower<-rownames(larson_domX)[which(larson_domX$direc=="-")]
+
+exp1_musY_higher<-rownames(exp1_musY)[which(exp1_musY$direc=="-")]
+exp2_musY_higher<-rownames(exp2_musY)[which(exp2_musY$direc=="+")]
+larson_musY_higher<-rownames(larson_musY)[which(larson_musY$direc=="-")]
+exp1_musY_lower<-rownames(exp1_musY)[which(exp1_musY$direc=="+")]
+exp2_musY_lower<-rownames(exp2_musY)[which(exp2_musY$direc=="-")]
+larson_musY_lower<-rownames(larson_musY)[which(larson_musY$direc=="+")]
+exp1_domY_higher<-rownames(exp1_domY)[which(exp1_domY$direc=="+")]
+exp2_domY_higher<-rownames(exp2_domY)[which(exp2_domY$direc=="-")]
+larson_domY_higher<-rownames(larson_domY)[which(larson_domY$direc=="+")]
+exp1_domY_lower<-rownames(exp1_domY)[which(exp1_domY$direc=="-")]
+exp2_domY_lower<-rownames(exp2_domY)[which(exp2_domY$direc=="+")]
+larson_domY_lower<-rownames(larson_domY)[which(larson_domY$direc=="-")]
+
+#DE in same direction, regardless of direction
+#This is going to be a bit complicated; need to remove only those genes that are DE in different directions but keep ones that are DE in one but not the other
+
+#Separate by chromosome type
+#Auto
+exp1_musX_higher_auto<-intersect(exp1_musX_higher, rownames(exp1_musX)[which(exp1_musX$chrs %in% c(1:19))])
+exp2_musX_higher_auto<-intersect(exp2_musX_higher, rownames(exp2_musX)[which(exp2_musX$chrs %in% c(1:19))])
+larson_musX_higher_auto<-intersect(larson_musX_higher, rownames(larson_musX)[which(larson_musX$chrs %in% c(1:19))])
+exp1_musX_lower_auto<-intersect(exp1_musX_lower, rownames(exp1_musX)[which(exp1_musX$chrs %in% c(1:19))])
+exp2_musX_lower_auto<-intersect(exp2_musX_lower, rownames(exp2_musX)[which(exp2_musX$chrs %in% c(1:19))])
+larson_musX_lower_auto<-intersect(larson_musX_lower, rownames(larson_musX)[which(larson_musX$chrs %in% c(1:19))])
+exp1_domX_higher_auto<-intersect(exp1_domX_higher, rownames(exp1_domX)[which(exp1_domX$chrs %in% c(1:19))])
+exp2_domX_higher_auto<-intersect(exp2_domX_higher, rownames(exp2_domX)[which(exp2_domX$chrs %in% c(1:19))])
+larson_domX_higher_auto<-intersect(larson_domX_higher, rownames(larson_domX)[which(larson_domX$chrs %in% c(1:19))])
+exp1_domX_lower_auto<-intersect(exp1_domX_lower, rownames(exp1_domX)[which(exp1_domX$chrs %in% c(1:19))])
+exp2_domX_lower_auto<-intersect(exp2_domX_lower, rownames(exp2_domX)[which(exp2_domX$chrs %in% c(1:19))])
+larson_domX_lower_auto<-intersect(larson_domX_lower, rownames(larson_domX)[which(larson_domX$chrs %in% c(1:19))])
+
+#X
+exp1_musX_higher_X<-intersect(exp1_musX_higher, rownames(exp1_musX)[which(exp1_musX$chrs=="X")])
+exp2_musX_higher_X<-intersect(exp2_musX_higher, rownames(exp2_musX)[which(exp2_musX$chrs=="X")])
+larson_musX_higher_X<-intersect(larson_musX_higher, rownames(larson_musX)[which(larson_musX$chrs=="X")])
+exp1_musX_lower_X<-intersect(exp1_musX_lower, rownames(exp1_musX)[which(exp1_musX$chrs=="X")])
+exp2_musX_lower_X<-intersect(exp2_musX_lower, rownames(exp2_musX)[which(exp2_musX$chrs=="X")])
+larson_musX_lower_X<-intersect(larson_musX_lower, rownames(larson_musX)[which(larson_musX$chrs=="X")])
+exp1_domX_higher_X<-intersect(exp1_domX_higher, rownames(exp1_domX)[which(exp1_domX$chrs=="X")])
+exp2_domX_higher_X<-intersect(exp2_domX_higher, rownames(exp2_domX)[which(exp2_domX$chrs=="X")])
+larson_domX_higher_X<-intersect(larson_domX_higher, rownames(larson_domX)[which(larson_domX$chrs=="X")])
+exp1_domX_lower_X<-intersect(exp1_domX_lower, rownames(exp1_domX)[which(exp1_domX$chrs=="X")])
+exp2_domX_lower_X<-intersect(exp2_domX_lower, rownames(exp2_domX)[which(exp2_domX$chrs=="X")])
+larson_domX_lower_X<-intersect(larson_domX_lower, rownames(larson_domX)[which(larson_domX$chrs=="X")])
+
+#Y
+exp1_musY_higher_Y<-intersect(exp1_musY_higher, rownames(exp1_musY)[which(exp1_musY$chrs=="Y")])
+exp2_musY_higher_Y<-intersect(exp2_musY_higher, rownames(exp2_musY)[which(exp2_musY$chrs=="Y")])
+larson_musY_higher_Y<-intersect(larson_musY_higher, rownames(larson_musY)[which(larson_musY$chrs=="Y")])
+exp1_musY_lower_Y<-intersect(exp1_musY_lower, rownames(exp1_musY)[which(exp1_musY$chrs=="Y")])
+exp2_musY_lower_Y<-intersect(exp2_musY_lower, rownames(exp2_musY)[which(exp2_musY$chrs=="Y")])
+larson_musY_lower_Y<-intersect(larson_musY_lower, rownames(larson_musY)[which(larson_musY$chrs=="Y")])
+exp1_domY_higher_Y<-intersect(exp1_domY_higher, rownames(exp1_domY)[which(exp1_domY$chrs=="Y")])
+exp2_domY_higher_Y<-intersect(exp2_domY_higher, rownames(exp2_domY)[which(exp2_domY$chrs=="Y")])
+larson_domY_higher_Y<-intersect(larson_domY_higher, rownames(larson_domY)[which(larson_domY$chrs=="Y")])
+exp1_domY_lower_Y<-intersect(exp1_domY_lower, rownames(exp1_domY)[which(exp1_domY$chrs=="Y")])
+exp2_domY_lower_Y<-intersect(exp2_domY_lower, rownames(exp2_domY)[which(exp2_domY$chrs=="Y")])
+larson_domY_lower_Y<-intersect(larson_domY_lower, rownames(larson_domY)[which(larson_domY$chrs=="Y")])
+
+#Plot
+print("Plotting...")
+pdf(paste("upset_plot",cell_type,"pdf", sep="."), onefile=TRUE)
+listInput_x_higher_auto<-list(mus_domYVSmus=exp1_musX_higher_auto, musXdomVSmusXdom_musY=exp2_musX_higher_auto, musXdomVSmus=larson_musX_higher_auto, dom_musYVSdom=exp1_domX_higher_auto, domXmusVSdomXmus_domY=exp2_domX_higher_auto, domXmusVSdom=larson_domX_higher_auto)
+upset(fromList(listInput_x_higher_auto), sets=names(listInput_x_higher_auto), keep.order=TRUE, mainbar.y.label="Number of DE genes - autos - higher in mismatch or hybrid") #order.by="freq"
+listInput_x_lower_auto<-list(mus_domYVSmus=exp1_musX_lower_auto, musXdomVSmusXdom_musY=exp2_musX_lower_auto, musXdomVSmus=larson_musX_lower_auto, dom_musYVSdom=exp1_domX_lower_auto, domXmusVSdomXmus_domY=exp2_domX_lower_auto, domXmusVSdom=larson_domX_lower_auto)
+upset(fromList(listInput_x_lower_auto), sets=names(listInput_x_lower_auto), keep.order=TRUE, mainbar.y.label="Number of DE genes - autos - lower in mismatch or hybrid")
+listInput_x_higher_X<-list(mus_domYVSmus=exp1_musX_higher_X, musXdomVSmusXdom_musY=exp2_musX_higher_X, musXdomVSmus=larson_musX_higher_X, dom_musYVSdom=exp1_domX_higher_X, domXmusVSdomXmus_domY=exp2_domX_higher_X, domXmusVSdom=larson_domX_higher_X)
+upset(fromList(listInput_x_higher_X), sets=names(listInput_x_higher_X), keep.order=TRUE, mainbar.y.label="Number of DE genes - X chr - higher in mismatch or hybrid")
+listInput_x_lower_X<-list(mus_domYVSmus=exp1_musX_lower_X, musXdomVSmusXdom_musY=exp2_musX_lower_X, musXdomVSmus=larson_musX_lower_X, dom_musYVSdom=exp1_domX_lower_X, domXmusVSdomXmus_domY=exp2_domX_lower_X, domXmusVSdom=larson_domX_lower_X)
+#If there are no DE genes in this category, list will have a character(0), which upset can't process, so remove them here
+listInput_x_lower_X_no0<-listInput_x_lower_X[lapply(listInput_x_lower_X, length)>0]
+upset(fromList(listInput_x_lower_X_no0), sets=names(listInput_x_lower_X_no0), keep.order=TRUE, mainbar.y.label="Number of DE genes - X chr - lower in mismatch or hybrid")
+listInput_y_higher_Y<-list(dom_musYVSmus=exp1_musY_higher_Y, domXmusVSmusXdom_musY=exp2_musY_higher_Y, domXmusVSmus=larson_musY_higher_Y, mus_domYVSdom=exp1_domY_higher_Y, musXdomVSdomXmus_domY=exp2_domY_higher_Y, musXdomVSdom=larson_domY_higher_Y)
+upset(fromList(listInput_y_higher_Y), sets=names(listInput_y_higher_Y), keep.order=TRUE, mainbar.y.label="Number of DE genes - Y chr - higher in mismatch or hybrid")
+listInput_y_lower_Y<-list(dom_musYVSmus=exp1_musY_lower_Y, domXmusVSmusXdom_musY=exp2_musY_lower_Y, domXmusVSmus=larson_musY_lower_Y, mus_domYVSdom=exp1_domY_lower_Y, musXdomVSdomXmus_domY=exp2_domY_lower_Y, musXdomVSdom=larson_domY_lower_Y)
+upset(fromList(listInput_y_lower_Y), sets=names(listInput_y_lower_Y), keep.order=TRUE, mainbar.y.label="Number of DE genes - Y chr - lower in mismatch or hybrid")
+dev.off()
